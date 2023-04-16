17e403eb9a0d5aa6503b7ca12b0e21fef39d3448
Hudson Junior <hudson_jun123@hotmail.com>
2023-04-16 16:44:21 -0300

    Fix img agora vai

--- src/components/CardProjects.vue
+++ src/components/CardProjects.vue
@@ -2,10 +2,10 @@
     <div class="flex justify-center ">
         <div class="block max-w-sm rounded-lg bg-gray shadow-lg dark:bg-neutral-700">
             <a class="flex justify-center" data-te-ripple-init data-te-ripple-color="light">
-            <img
-                class="rounded-t-lg"
-                :src="props.img"
-                :alt="props.alt" />
+                <img
+                    class="rounded-t-lg"
+                    :src="getImageUrl()"
+                    :alt="props.alt" />
             </a>
             <div class="p-5">
                 <h5
@@ -28,14 +28,21 @@
     </div>
 </template>
 <script setup lang="ts">
+import {defineProps} from "vue";
 
 const props = defineProps({
     link: String,
-    img: String,
+    imageUrl: String,
     alt: String,
     titulo: String,
-    descricao: String,
+    descricao: String
 })
+
+
+function getImageUrl():any {
+  // This path must be correct for your file
+  return new URL(`../assets/${props.imageUrl}`, import.meta.url)
+}
 
 </script>
 <style scoped>
